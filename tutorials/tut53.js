af1044584e2c9a31f5694101e49557776dcf0831
Rohan <rohan.uprety@siemens.com>
2021-03-30 12:26:52 +0530

    JS tutorials

---
+++ tutorials/tut53.js
@@ -0,0 +1,16 @@
+console.log("This is tutorial 53");
+
+function greet(name, greetText="Greetings from JavaScript"){
+    console.log(greetText + " " + name);
+    console.log(name "is a good boy");
+}
+
+greet("ABC");
+greet("Rohan", "Good Morning");
+
+function sum(a,b,c) {
+    return a+b+c;
+}
+
+let result = sum(1,2,3);
+console.log(result);
